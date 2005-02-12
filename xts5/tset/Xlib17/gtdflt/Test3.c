6d85c7ba876bbc5e61eb3f88053c910463640c75
anderson <anderson>
2005-02-12 15:34:34 +0000

    Don't expect the 3rd arg to tet_main() to be envp

--- xts5/tset/Xlib17/gtdflt/Test3.c
+++ xts5/tset/Xlib17/gtdflt/Test3.c
@@ -20,7 +20,7 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 */
 /*
-* $Header: /cvs/xtest/xtest/xts5/tset/Xlib17/gtdflt/Test3.c,v 1.1 2005-02-12 14:37:30 anderson Exp $
+* $Header: /cvs/xtest/xtest/xts5/tset/Xlib17/gtdflt/Test3.c,v 1.2 2005-02-12 15:34:34 anderson Exp $
 * 
 * Copyright (c) Applied Testing and Technology, Inc. 1995
 * All Rights Reserved.
@@ -34,8 +34,12 @@ SOFTWARE.
 * 
 * Modifications:
 * $Log: Test3.c,v $
-* Revision 1.1  2005-02-12 14:37:30  anderson
-* Initial revision
+* Revision 1.2  2005-02-12 15:34:34  anderson
+* Don't expect the 3rd arg to tet_main() to be envp
+*
+* Revision 1.1.1.1  2005/02/12 14:37:30  anderson
+* VSW5 Source under an MIT license This is version 5.0.2 as received from
+* AppTest with the new license applied.
 *
 * Revision 8.0  1998/12/23 23:34:37  mar
 * Branch point for Release 5.0.2
@@ -122,10 +126,9 @@ struct tet_testlist tet_testlist[] = {
 };
 int 	ntests = sizeof(tet_testlist)/sizeof(struct tet_testlist)-1;
 
-tet_main(argc, argv, envp)
+tet_main(argc, argv)
 int argc;	
 char *argv[];
-char *envp[];
 {
 int		pass = 0, fail = 0;
 Display		*display;
