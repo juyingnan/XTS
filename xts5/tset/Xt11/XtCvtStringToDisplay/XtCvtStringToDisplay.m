1282cafdeb8bfc244c930be04cb0c9c1425df46a
Peter Hutterer <peter.hutterer@who-t.net>
2009-09-10 13:29:04 +1000

    Renaming tcstdisp/ to XtCvtStringToDisplay

--- xts5/tset/Xt11/XtCvtStringToDisplay/XtCvtStringToDisplay.m
+++ xts5/tset/Xt11/XtCvtStringToDisplay/XtCvtStringToDisplay.m
@@ -17,7 +17,7 @@ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
 LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
-$Header: /cvs/xtest/xtest/xts5/tset/Xt11/tcstdisp/tcstdisp.m,v 1.1 2005-02-12 14:37:52 anderson Exp $
+$Header: /cvs/xtest/xtest/xts5/tset/Xt11/XtCvtStringToDisplay/XtCvtStringToDisplay.m,v 1.1 2005-02-12 14:37:52 anderson Exp $
 
 Copyright (c) Applied Testing and Technology, Inc. 1993, 1994, 1995
 Copyright (c) 88open Consortium, Ltd. 1990, 1991, 1992, 1993
@@ -26,7 +26,7 @@ All Rights Reserved.
 >># 
 >># Project: VSW5
 >># 
->># File: tset/Xt11/tcstdisp/tcstdisp.m
+>># File: tset/Xt11/XtCvtStringToDisplay/XtCvtStringToDisplay.m
 >># 
 >># Description:
 >>#	Tests for XtCvtStringToDisplay()
