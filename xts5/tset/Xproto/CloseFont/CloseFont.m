ecf91db0a9fbb10ca92cf22d55067a7361c6ada7
Peter Hutterer <peter.hutterer@who-t.net>
2009-09-10 12:22:55 +1000

    Renaming clsfnt/ to CloseFont

--- xts5/tset/Xproto/CloseFont/CloseFont.m
+++ xts5/tset/Xproto/CloseFont/CloseFont.m
@@ -17,14 +17,14 @@ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
 LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
-$Header: /cvs/xtest/xtest/xts5/tset/Xproto/clsfnt/clsfnt.m,v 1.2 2005-11-03 08:44:03 jmichael Exp $
+$Header: /cvs/xtest/xtest/xts5/tset/Xproto/CloseFont/CloseFont.m,v 1.2 2005-11-03 08:44:03 jmichael Exp $
 
 Copyright (c) Applied Testing and Technology, Inc. 1995
 All Rights Reserved.
 
 >># Project: VSW5
 >># 
->># File: xts5/tset/Xproto/clsfnt/clsfnt.m
+>># File: xts5/tset/Xproto/CloseFont/CloseFont.m
 >># 
 >># Description:
 >># 	Tests for CloseFont
