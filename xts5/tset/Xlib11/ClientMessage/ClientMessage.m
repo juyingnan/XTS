0f9a3084c49a54a6c757b15086fa67d9263db8b2
Peter Hutterer <peter.hutterer@who-t.net>
2009-09-10 12:54:46 +1000

    Renaming clntmssg/ to ClientMessage

--- xts5/tset/Xlib11/ClientMessage/ClientMessage.m
+++ xts5/tset/Xlib11/ClientMessage/ClientMessage.m
@@ -17,14 +17,14 @@ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
 LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
-$Header: /cvs/xtest/xtest/xts5/tset/Xlib11/clntmssg/clntmssg.m,v 1.2 2005-11-03 08:42:20 jmichael Exp $
+$Header: /cvs/xtest/xtest/xts5/tset/Xlib11/ClientMessage/ClientMessage.m,v 1.2 2005-11-03 08:42:20 jmichael Exp $
 
 Copyright (c) Applied Testing and Technology, Inc. 1995
 All Rights Reserved.
 
 >># Project: VSW5
 >># 
->># File: xts5/tset/Xlib11/clntmssg/clntmssg.m
+>># File: xts5/tset/Xlib11/ClientMessage/ClientMessage.m
 >># 
 >># Description:
 >># 	Tests for ClientMessage()
