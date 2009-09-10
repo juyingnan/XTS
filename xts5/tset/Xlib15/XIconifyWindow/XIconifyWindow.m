17f2026e9323b2df1cbeaef95fc7f0670a0d2bdc
Peter Hutterer <peter.hutterer@who-t.net>
2009-09-10 13:22:07 +1000

    Renaming icnfywdw/ to XIconifyWindow

--- xts5/tset/Xlib15/XIconifyWindow/XIconifyWindow.m
+++ xts5/tset/Xlib15/XIconifyWindow/XIconifyWindow.m
@@ -17,14 +17,14 @@ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
 LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
-$Header: /cvs/xtest/xtest/xts5/tset/Xlib15/icnfywdw/icnfywdw.m,v 1.2 2005-11-03 08:42:50 jmichael Exp $
+$Header: /cvs/xtest/xtest/xts5/tset/Xlib15/XIconifyWindow/XIconifyWindow.m,v 1.2 2005-11-03 08:42:50 jmichael Exp $
 
 Copyright (c) Applied Testing and Technology, Inc. 1995
 All Rights Reserved.
 
 >># Project: VSW5
 >># 
->># File: xts5/tset/Xlib15/icnfywdw/icnfywdw.m
+>># File: xts5/tset/Xlib15/XIconifyWindow/XIconifyWindow.m
 >># 
 >># Description:
 >># 	Tests for XIconifyWindow()
