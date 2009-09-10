eb83355a628a0fa840e86bf5cf831a2b3f3e1db1
Peter Hutterer <peter.hutterer@who-t.net>
2009-09-10 13:27:27 +1000

    Renaming vcnlist/ to XVaCreateNestedList

--- xts5/tset/Xlib14/XVaCreateNestedList/XVaCreateNestedList.m
+++ xts5/tset/Xlib14/XVaCreateNestedList/XVaCreateNestedList.m
@@ -17,14 +17,14 @@ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
 LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
-$Header: /cvs/xtest/xtest/xts5/tset/Xlib14/vcnlist/vcnlist.m,v 1.3 2006-06-06 14:41:29 gwc Exp $
+$Header: /cvs/xtest/xtest/xts5/tset/Xlib14/XVaCreateNestedList/XVaCreateNestedList.m,v 1.3 2006-06-06 14:41:29 gwc Exp $
 
 Copyright (c) Applied Testing and Technology, Inc. 1995
 All Rights Reserved.
 
 >># Project: VSW5
 >># 
->># File: xts5/tset/Xlib14/vcnlist/vcnlist.m
+>># File: xts5/tset/Xlib14/XVaCreateNestedList/XVaCreateNestedList.m
 >># 
 >># Description:
 >># 	Tests for XVaCreateNestedList()
