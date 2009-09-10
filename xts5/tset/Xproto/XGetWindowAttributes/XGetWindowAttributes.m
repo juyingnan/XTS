5c79285cfb404ec7018dc3c1162038a2e107862a
Peter Hutterer <peter.hutterer@who-t.net>
2009-09-10 13:06:59 +1000

    Renaming GetWindowAttributes/ to XGetWindowAttributes

--- xts5/tset/Xproto/XGetWindowAttributes/XGetWindowAttributes.m
+++ xts5/tset/Xproto/XGetWindowAttributes/XGetWindowAttributes.m
@@ -17,14 +17,14 @@ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
 LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
-$Header: /cvs/xtest/xtest/xts5/tset/Xproto/GetWindowAttributes/GetWindowAttributes.m,v 1.2 2005-11-03 08:44:10 jmichael Exp $
+$Header: /cvs/xtest/xtest/xts5/tset/Xproto/XGetWindowAttributes/XGetWindowAttributes.m,v 1.2 2005-11-03 08:44:10 jmichael Exp $
 
 Copyright (c) Applied Testing and Technology, Inc. 1995
 All Rights Reserved.
 
 >># Project: VSW5
 >># 
->># File: xts5/tset/Xproto/GetWindowAttributes/GetWindowAttributes.m
+>># File: xts5/tset/Xproto/XGetWindowAttributes/XGetWindowAttributes.m
 >># 
 >># Description:
 >># 	Tests for GetWindowAttributes
