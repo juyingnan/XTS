cff716b4032dc35dd7747ec225a493a15ac4b0c8
Peter Hutterer <peter.hutterer@who-t.net>
2009-09-10 13:29:31 +1000

    Renaming tkytoklst/ to XtKeysymToKeycodeList

--- xts5/tset/Xt12/XtKeysymToKeycodeList/XtKeysymToKeycodeList.m
+++ xts5/tset/Xt12/XtKeysymToKeycodeList/XtKeysymToKeycodeList.m
@@ -17,7 +17,7 @@ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
 LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
-$Header: /cvs/xtest/xtest/xts5/tset/Xt12/tkytoklst/tkytoklst.m,v 1.1 2005-02-12 14:37:56 anderson Exp $
+$Header: /cvs/xtest/xtest/xts5/tset/Xt12/XtKeysymToKeycodeList/XtKeysymToKeycodeList.m,v 1.1 2005-02-12 14:37:56 anderson Exp $
 
 Copyright (c) Applied Testing and Technology, Inc. 1993, 1994, 1995
 Copyright (c) 88open Consortium, Ltd. 1990, 1991, 1992, 1993
@@ -26,7 +26,7 @@ All Rights Reserved.
 >># 
 >># Project: VSW5
 >># 
->># File: tset/Xt12/tkytoklst/tkytoklst.m
+>># File: tset/Xt12/XtKeysymToKeycodeList/XtKeysymToKeycodeList.m
 >># 
 >># Description:
 >>#	Tests for XtKeysymToKeycodeList()
