4e2cd80b6dfc1e2e3c8b4c647aed1352748e37ea
Peter Hutterer <peter.hutterer@who-t.net>
2009-09-10 13:12:30 +1000

    Renaming FreeGC/ to XFreeGC

--- xts5/tset/Xlib8/XFreeGC/XFreeGC.m
+++ xts5/tset/Xlib8/XFreeGC/XFreeGC.m
@@ -17,14 +17,14 @@ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
 LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
-$Header: /cvs/xtest/xtest/xts5/tset/Xlib8/FreeGC/FreeGC.m,v 1.2 2005-11-03 08:43:47 jmichael Exp $
+$Header: /cvs/xtest/xtest/xts5/tset/Xlib8/XFreeGC/XFreeGC.m,v 1.2 2005-11-03 08:43:47 jmichael Exp $
 
 Copyright (c) Applied Testing and Technology, Inc. 1995
 All Rights Reserved.
 
 >># Project: VSW5
 >># 
->># File: xts5/tset/Xlib8/FreeGC/FreeGC.m
+>># File: xts5/tset/Xlib8/XFreeGC/XFreeGC.m
 >># 
 >># Description:
 >># 	Tests for XFreeGC()
