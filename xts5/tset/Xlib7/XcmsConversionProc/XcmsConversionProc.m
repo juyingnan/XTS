75fabe4a2a6695b7e43e1d62cd72151a71bea7a5
Peter Hutterer <peter.hutterer@who-t.net>
2009-09-10 13:08:59 +1000

    Renaming cmscproc/ to XcmsConversionProc

--- xts5/tset/Xlib7/XcmsConversionProc/XcmsConversionProc.m
+++ xts5/tset/Xlib7/XcmsConversionProc/XcmsConversionProc.m
@@ -17,7 +17,7 @@ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
 LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
-$Header: /cvs/xtest/xtest/xts5/tset/Xlib7/cmscproc/cmscproc.m,v 1.1 2005-02-12 14:37:35 anderson Exp $
+$Header: /cvs/xtest/xtest/xts5/tset/Xlib7/XcmsConversionProc/XcmsConversionProc.m,v 1.1 2005-02-12 14:37:35 anderson Exp $
 
 Copyright (c) Applied Testing and Technology, Inc. 1993, 1994, 1995
 Copyright (c) 88open Consortium, Ltd. 1990, 1991, 1992, 1993
@@ -26,7 +26,7 @@ All Rights Reserved.
 >># 
 >># Project: VSW5
 >># 
->># File: tset/Xlib7/cmscproc/cmscproc.m
+>># File: tset/Xlib7/XcmsConversionProc/XcmsConversionProc.m
 >># 
 >># Description:
 >>#	Tests for XcmsConversionProc()
