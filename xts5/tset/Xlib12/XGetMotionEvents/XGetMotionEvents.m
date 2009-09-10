43b139ed5cbfa646140debc59516f7779c483a89
Peter Hutterer <peter.hutterer@who-t.net>
2009-09-10 12:56:43 +1000

    Renaming GetMotionEvents/ to XGetMotionEvents

--- xts5/tset/Xlib12/XGetMotionEvents/XGetMotionEvents.m
+++ xts5/tset/Xlib12/XGetMotionEvents/XGetMotionEvents.m
@@ -17,14 +17,14 @@ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
 LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
-$Header: /cvs/xtest/xtest/xts5/tset/Xlib12/GetMotionEvents/GetMotionEvents.m,v 1.2 2005-11-03 08:42:35 jmichael Exp $
+$Header: /cvs/xtest/xtest/xts5/tset/Xlib12/XGetMotionEvents/XGetMotionEvents.m,v 1.2 2005-11-03 08:42:35 jmichael Exp $
 
 Copyright (c) Applied Testing and Technology, Inc. 1995
 All Rights Reserved.
 
 >># Project: VSW5
 >># 
->># File: xts5/tset/Xlib12/GetMotionEvents/GetMotionEvents.m
+>># File: xts5/tset/Xlib12/XGetMotionEvents/XGetMotionEvents.m
 >># 
 >># Description:
 >># 	Tests for XGetMotionEvents()
