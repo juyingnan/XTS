415ee25af34c07b81ab82013b50b698058f4f8b9
Peter Hutterer <peter.hutterer@who-t.net>
2009-09-10 13:31:07 +1000

    Renaming tpopup/ to XtPopup

--- xts5/tset/Xt7/XtPopup/XtPopup.m
+++ xts5/tset/Xt7/XtPopup/XtPopup.m
@@ -17,7 +17,7 @@ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
 LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
-$Header: /cvs/xtest/xtest/xts5/tset/Xt7/tpopup/tpopup.m,v 1.2 2005-04-21 09:40:42 ajosey Exp $
+$Header: /cvs/xtest/xtest/xts5/tset/Xt7/XtPopup/XtPopup.m,v 1.2 2005-04-21 09:40:42 ajosey Exp $
 
 Copyright (c) 2004 The Open Group
 Copyright (c) Applied Testing and Technology, Inc. 1993, 1994, 1995
@@ -27,7 +27,7 @@ All Rights Reserved.
 >># 
 >># Project: VSW5
 >># 
->># File: tset/Xt7/tpopup/tpopup.m
+>># File: tset/Xt7/XtPopup/XtPopup.m
 >># 
 >># Description:
 >>#	Tests for XtPopup()
