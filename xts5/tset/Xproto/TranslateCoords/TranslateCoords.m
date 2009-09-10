1cfa68ef9c88679c27fb2fed8e3404eb321f4564
Peter Hutterer <peter.hutterer@who-t.net>
2009-09-10 12:24:04 +1000

    Renaming trnsltcrds/ to TranslateCoords

--- xts5/tset/Xproto/TranslateCoords/TranslateCoords.m
+++ xts5/tset/Xproto/TranslateCoords/TranslateCoords.m
@@ -17,14 +17,14 @@ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
 LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
-$Header: /cvs/xtest/xtest/xts5/tset/Xproto/trnsltcrds/trnsltcrds.m,v 1.2 2005-11-03 08:44:22 jmichael Exp $
+$Header: /cvs/xtest/xtest/xts5/tset/Xproto/TranslateCoords/TranslateCoords.m,v 1.2 2005-11-03 08:44:22 jmichael Exp $
 
 Copyright (c) Applied Testing and Technology, Inc. 1995
 All Rights Reserved.
 
 >># Project: VSW5
 >># 
->># File: xts5/tset/Xproto/trnsltcrds/trnsltcrds.m
+>># File: xts5/tset/Xproto/TranslateCoords/TranslateCoords.m
 >># 
 >># Description:
 >># 	Tests for TranslateCoords
