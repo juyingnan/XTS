25a726e8b68f744deb5a103ff647159cd3a2fee2
Peter Hutterer <peter.hutterer@who-t.net>
2009-09-10 13:15:44 +1000

    Renaming ClearArea/ to XClearArea

--- xts5/tset/Xproto/XClearArea/XClearArea.m
+++ xts5/tset/Xproto/XClearArea/XClearArea.m
@@ -17,14 +17,14 @@ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
 LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
-$Header: /cvs/xtest/xtest/xts5/tset/Xproto/ClearArea/ClearArea.m,v 1.2 2005-11-03 08:44:03 jmichael Exp $
+$Header: /cvs/xtest/xtest/xts5/tset/Xproto/XClearArea/XClearArea.m,v 1.2 2005-11-03 08:44:03 jmichael Exp $
 
 Copyright (c) Applied Testing and Technology, Inc. 1995
 All Rights Reserved.
 
 >># Project: VSW5
 >># 
->># File: xts5/tset/Xproto/ClearArea/ClearArea.m
+>># File: xts5/tset/Xproto/XClearArea/XClearArea.m
 >># 
 >># Description:
 >># 	Tests for ClearArea
