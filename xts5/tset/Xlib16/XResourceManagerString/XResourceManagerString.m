8e89e38fcd35a84ce240972f6896be1c10eb43f2
Peter Hutterer <peter.hutterer@who-t.net>
2009-09-10 13:21:01 +1000

    Renaming rsrcmngrst/ to XResourceManagerString

--- xts5/tset/Xlib16/XResourceManagerString/XResourceManagerString.m
+++ xts5/tset/Xlib16/XResourceManagerString/XResourceManagerString.m
@@ -17,14 +17,14 @@ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
 LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
-$Header: /cvs/xtest/xtest/xts5/tset/Xlib16/rsrcmngrst/rsrcmngrst.m,v 1.2 2005-11-03 08:43:02 jmichael Exp $
+$Header: /cvs/xtest/xtest/xts5/tset/Xlib16/XResourceManagerString/XResourceManagerString.m,v 1.2 2005-11-03 08:43:02 jmichael Exp $
 
 Copyright (c) Applied Testing and Technology, Inc. 1995
 All Rights Reserved.
 
 >># Project: VSW5
 >># 
->># File: xts5/tset/Xlib16/rsrcmngrst/rsrcmngrst.m
+>># File: xts5/tset/Xlib16/XResourceManagerString/XResourceManagerString.m
 >># 
 >># Description:
 >># 	Tests for XResourceManagerString()
