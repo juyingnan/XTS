ea1a45ba7e0b5877c6cbe0050d9d6bdcb330e020
Peter Hutterer <peter.hutterer@who-t.net>
2009-09-10 13:29:22 +1000

    Renaming tvastvals/ to XtVaSetValues

--- xts5/tset/Xt11/XtVaSetValues/XtVaSetValues.m
+++ xts5/tset/Xt11/XtVaSetValues/XtVaSetValues.m
@@ -17,7 +17,7 @@ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
 LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
-$Header: /cvs/xtest/xtest/xts5/tset/Xt11/tvastvals/tvastvals.m,v 1.1 2005-02-12 14:37:55 anderson Exp $
+$Header: /cvs/xtest/xtest/xts5/tset/Xt11/XtVaSetValues/XtVaSetValues.m,v 1.1 2005-02-12 14:37:55 anderson Exp $
 
 Copyright (c) Applied Testing and Technology, Inc. 1993, 1994, 1995
 Copyright (c) 88open Consortium, Ltd. 1990, 1991, 1992, 1993
@@ -26,7 +26,7 @@ All Rights Reserved.
 >># 
 >># Project: VSW5
 >># 
->># File: tset/Xt11/tvastvals/tvastvals.m
+>># File: tset/Xt11/XtVaSetValues/XtVaSetValues.m
 >># 
 >># Description:
 >>#	Tests for XtVaSetValues()
