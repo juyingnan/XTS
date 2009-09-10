ca01fca6580b65ca548018794b9c97e4192cdef0
Peter Hutterer <peter.hutterer@who-t.net>
2009-09-10 13:22:17 +1000

    Renaming stwmprprts/ to XSetWMProperties

--- xts5/tset/Xlib15/XSetWMProperties/Test2.c
+++ xts5/tset/Xlib15/XSetWMProperties/Test2.c
@@ -20,14 +20,14 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 */
 /*
-* $Header: /cvs/xtest/xtest/xts5/tset/Xlib15/stwmprprts/Test2.c,v 1.5 2005-11-03 08:42:54 jmichael Exp $
+* $Header: /cvs/xtest/xtest/xts5/tset/Xlib15/XSetWMProperties/Test2.c,v 1.5 2005-11-03 08:42:54 jmichael Exp $
 * 
 * Copyright (c) Applied Testing and Technology, Inc. 1995
 * All Rights Reserved.
 * 
 * Project: VSW5
 * 
-* File: xts5/tset/Xlib15/stwmprprts/Test2.c
+* File: xts5/tset/Xlib15/XSetWMProperties/Test2.c
 * 
 * Description:
 * 	Tests for XSetWMProperties()
