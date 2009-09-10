46d9309105b01318b6baa42cf9d13461e8bca2b1
Peter Hutterer <peter.hutterer@who-t.net>
2009-09-10 12:54:00 +1000

    Renaming enblaccssc/ to XEnableAccessControl

--- xts5/tset/Xlib10/XEnableAccessControl/XEnableAccessControl.m
+++ xts5/tset/Xlib10/XEnableAccessControl/XEnableAccessControl.m
@@ -17,14 +17,14 @@ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
 LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
-$Header: /cvs/xtest/xtest/xts5/tset/Xlib10/enblaccssc/enblaccssc.m,v 1.2 2005-11-03 08:42:16 jmichael Exp $
+$Header: /cvs/xtest/xtest/xts5/tset/Xlib10/XEnableAccessControl/XEnableAccessControl.m,v 1.2 2005-11-03 08:42:16 jmichael Exp $
 
 Copyright (c) Applied Testing and Technology, Inc. 1995
 All Rights Reserved.
 
 >># Project: VSW5
 >># 
->># File: xts5/tset/Xlib10/enblaccssc/enblaccssc.m
+>># File: xts5/tset/Xlib10/XEnableAccessControl/XEnableAccessControl.m
 >># 
 >># Description:
 >># 	Tests for XEnableAccessControl()
