83fa20bcacf85a93107d0185505502f43ff0a43d
Peter Hutterer <peter.hutterer@who-t.net>
2009-09-10 13:27:32 +1000

    Renaming wctesc/ to XwcTextEscapement

--- xts5/tset/Xlib14/XwcTextEscapement/XwcTextEscapement.m
+++ xts5/tset/Xlib14/XwcTextEscapement/XwcTextEscapement.m
@@ -17,14 +17,14 @@ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
 LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
-$Header: /cvs/xtest/xtest/xts5/tset/Xlib14/wctesc/wctesc.m,v 1.2 2005-11-03 08:42:46 jmichael Exp $
+$Header: /cvs/xtest/xtest/xts5/tset/Xlib14/XwcTextEscapement/XwcTextEscapement.m,v 1.2 2005-11-03 08:42:46 jmichael Exp $
 
 Copyright (c) Applied Testing and Technology, Inc. 1995
 All Rights Reserved.
 
 >># Project: VSW5
 >># 
->># File: xts5/tset/Xlib14/wctesc/wctesc.m
+>># File: xts5/tset/Xlib14/XwcTextEscapement/XwcTextEscapement.m
 >># 
 >># Description:
 >># 	Tests for XwcTextEscapement()
