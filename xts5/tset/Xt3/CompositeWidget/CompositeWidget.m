b2ee9d2959cdcd2a8470080ce1e460785fcd1894
Peter Hutterer <peter.hutterer@who-t.net>
2009-09-10 13:30:20 +1000

    Renaming wdgtcmpst/ to CompositeWidget

--- xts5/tset/Xt3/CompositeWidget/CompositeWidget.m
+++ xts5/tset/Xt3/CompositeWidget/CompositeWidget.m
@@ -17,7 +17,7 @@ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
 LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
-$Header: /cvs/xtest/xtest/xts5/tset/Xt3/wdgtcmpst/wdgtcmpst.m,v 1.1 2005-02-12 14:38:00 anderson Exp $
+$Header: /cvs/xtest/xtest/xts5/tset/Xt3/CompositeWidget/CompositeWidget.m,v 1.1 2005-02-12 14:38:00 anderson Exp $
 
 Copyright (c) Applied Testing and Technology, Inc. 1993, 1994, 1995
 Copyright (c) 88open Consortium, Ltd. 1990, 1991, 1992, 1993
@@ -26,7 +26,7 @@ All Rights Reserved.
 >>#
 >># Project: VSW5
 >>#
->># File: tset/Xt3/wdgtcmpst/wdgtcmpst.m
+>># File: tset/Xt3/CompositeWidget/CompositeWidget.m
 >>#
 >># Description:
 >>#	 Tests for Composite widget data structures
