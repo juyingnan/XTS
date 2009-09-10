e30b54b5b075697673b6549a813ec98399f303cc
Peter Hutterer <peter.hutterer@who-t.net>
2009-09-10 13:22:19 +1000

    Renaming txtprprtyt/ to XTextPropertyToStringList

--- xts5/tset/Xlib15/XTextPropertyToStringList/XTextPropertyToStringList.m
+++ xts5/tset/Xlib15/XTextPropertyToStringList/XTextPropertyToStringList.m
@@ -17,14 +17,14 @@ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
 LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
-$Header: /cvs/xtest/xtest/xts5/tset/Xlib15/txtprprtyt/txtprprtyt.m,v 1.2 2005-11-03 08:42:54 jmichael Exp $
+$Header: /cvs/xtest/xtest/xts5/tset/Xlib15/XTextPropertyToStringList/XTextPropertyToStringList.m,v 1.2 2005-11-03 08:42:54 jmichael Exp $
 
 Copyright (c) Applied Testing and Technology, Inc. 1995
 All Rights Reserved.
 
 >># Project: VSW5
 >># 
->># File: xts5/tset/Xlib15/txtprprtyt/txtprprtyt.m
+>># File: xts5/tset/Xlib15/XTextPropertyToStringList/XTextPropertyToStringList.m
 >># 
 >># Description:
 >># 	Tests for XTextPropertyToStringList()
