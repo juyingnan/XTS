3dd69bdce09f3c75535c70846a63b867e7bfeb1d
Peter Hutterer <peter.hutterer@who-t.net>
2009-09-10 12:23:58 +1000

    Renaming stfntpth/ to SetFontPath

--- xts5/tset/Xlib9/SetFontPath/SetFontPath.m
+++ xts5/tset/Xlib9/SetFontPath/SetFontPath.m
@@ -17,14 +17,14 @@ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
 LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
-$Header: /cvs/xtest/xtest/xts5/tset/Xlib9/stfntpth/stfntpth.m,v 1.2 2005-11-03 08:43:59 jmichael Exp $
+$Header: /cvs/xtest/xtest/xts5/tset/Xlib9/SetFontPath/SetFontPath.m,v 1.2 2005-11-03 08:43:59 jmichael Exp $
 
 Copyright (c) Applied Testing and Technology, Inc. 1995
 All Rights Reserved.
 
 >># Project: VSW5
 >># 
->># File: xts5/tset/Xlib9/stfntpth/stfntpth.m
+>># File: xts5/tset/Xlib9/SetFontPath/SetFontPath.m
 >># 
 >># Description:
 >># 	Tests for XSetFontPath()
