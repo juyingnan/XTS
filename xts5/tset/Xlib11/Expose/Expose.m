3b0d37e8026e1618c231487ceb97e0f94a1cde1a
Peter Hutterer <peter.hutterer@who-t.net>
2009-09-10 12:54:51 +1000

    Renaming exps/ to Expose

--- xts5/tset/Xlib11/Expose/Expose.m
+++ xts5/tset/Xlib11/Expose/Expose.m
@@ -17,14 +17,14 @@ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
 LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
-$Header: /cvs/xtest/xtest/xts5/tset/Xlib11/exps/exps.m,v 1.2 2005-11-03 08:42:27 jmichael Exp $
+$Header: /cvs/xtest/xtest/xts5/tset/Xlib11/Expose/Expose.m,v 1.2 2005-11-03 08:42:27 jmichael Exp $
 
 Copyright (c) Applied Testing and Technology, Inc. 1995
 All Rights Reserved.
 
 >># Project: VSW5
 >># 
->># File: xts5/tset/Xlib11/exps/exps.m
+>># File: xts5/tset/Xlib11/Expose/Expose.m
 >># 
 >># Description:
 >># 	Tests for Expose()
