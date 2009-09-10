5ec0f1ea974527be7eca2454ff45a89d82f04dc0
Peter Hutterer <peter.hutterer@who-t.net>
2009-09-10 13:29:19 +1000

    Renaming tvagtares/ to XtVaGetApplicationResources

--- xts5/tset/Xt11/XtVaGetApplicationResources/XtVaGetApplicationResources.m
+++ xts5/tset/Xt11/XtVaGetApplicationResources/XtVaGetApplicationResources.m
@@ -17,7 +17,7 @@ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
 LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
-$Header: /cvs/xtest/xtest/xts5/tset/Xt11/tvagtares/tvagtares.m,v 1.1 2005-02-12 14:37:54 anderson Exp $
+$Header: /cvs/xtest/xtest/xts5/tset/Xt11/XtVaGetApplicationResources/XtVaGetApplicationResources.m,v 1.1 2005-02-12 14:37:54 anderson Exp $
 
 Copyright (c) Applied Testing and Technology, Inc. 1993, 1994, 1995
 Copyright (c) 88open Consortium, Ltd. 1990, 1991, 1992, 1993
@@ -26,7 +26,7 @@ All Rights Reserved.
 >># 
 >># Project: VSW5
 >># 
->># File: tset/Xt11/tvagtares/tvagtares.m
+>># File: tset/Xt11/XtVaGetApplicationResources/XtVaGetApplicationResources.m
 >># 
 >># Description:
 >>#	Tests for XtVaGetApplicationResources()
