2d8d56a00279cf531af0579ca9f939cbf0e65154
Peter Hutterer <peter.hutterer@who-t.net>
2009-09-10 13:27:17 +1000

    Renaming mbdtext/ to XmbDrawText

--- xts5/tset/Xlib14/XmbDrawText/XmbDrawText.m
+++ xts5/tset/Xlib14/XmbDrawText/XmbDrawText.m
@@ -17,14 +17,14 @@ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
 LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
-$Header: /cvs/xtest/xtest/xts5/tset/Xlib14/mbdtext/mbdtext.m,v 1.2 2005-11-03 08:42:44 jmichael Exp $
+$Header: /cvs/xtest/xtest/xts5/tset/Xlib14/XmbDrawText/XmbDrawText.m,v 1.2 2005-11-03 08:42:44 jmichael Exp $
 
 Copyright (c) Applied Testing and Technology, Inc. 1995
 All Rights Reserved.
 
 >># Project: VSW5
 >># 
->># File: xts5/tset/Xlib14/mbdtext/mbdtext.m
+>># File: xts5/tset/Xlib14/XmbDrawText/XmbDrawText.m
 >># 
 >># Description:
 >># 	Tests for XmbDrawText()
