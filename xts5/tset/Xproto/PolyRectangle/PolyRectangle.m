02f317f7470ca7dd7c65592c515628ef85debb23
Peter Hutterer <peter.hutterer@who-t.net>
2009-09-10 12:23:45 +1000

    Renaming plyrctngl/ to PolyRectangle

--- xts5/tset/Xproto/PolyRectangle/PolyRectangle.m
+++ xts5/tset/Xproto/PolyRectangle/PolyRectangle.m
@@ -17,14 +17,14 @@ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
 LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
-$Header: /cvs/xtest/xtest/xts5/tset/Xproto/plyrctngl/plyrctngl.m,v 1.2 2005-11-03 08:44:15 jmichael Exp $
+$Header: /cvs/xtest/xtest/xts5/tset/Xproto/PolyRectangle/PolyRectangle.m,v 1.2 2005-11-03 08:44:15 jmichael Exp $
 
 Copyright (c) Applied Testing and Technology, Inc. 1995
 All Rights Reserved.
 
 >># Project: VSW5
 >># 
->># File: xts5/tset/Xproto/plyrctngl/plyrctngl.m
+>># File: xts5/tset/Xproto/PolyRectangle/PolyRectangle.m
 >># 
 >># Description:
 >># 	Tests for PolyRectangle
