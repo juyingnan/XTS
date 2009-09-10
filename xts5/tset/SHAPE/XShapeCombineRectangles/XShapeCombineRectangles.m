cff25f664eb137c82f84d25e2d5d038d21b5546e
Peter Hutterer <peter.hutterer@who-t.net>
2009-09-10 12:31:02 +1000

    Renaming tshpcrect/ to XShapeCombineRectangles

--- xts5/tset/SHAPE/XShapeCombineRectangles/XShapeCombineRectangles.m
+++ xts5/tset/SHAPE/XShapeCombineRectangles/XShapeCombineRectangles.m
@@ -17,7 +17,7 @@ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
 LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
-$Header: /cvs/xtest/xtest/xts5/tset/SHAPE/tshpcrect/tshpcrect.m,v 1.1 2005-02-12 14:37:16 anderson Exp $
+$Header: /cvs/xtest/xtest/xts5/tset/SHAPE/XShapeCombineRectangles/XShapeCombineRectangles.m,v 1.1 2005-02-12 14:37:16 anderson Exp $
 
 Copyright (c) Applied Testing and Technology, Inc. 1993, 1994, 1995
 Copyright (c) 88open Consortium, Ltd. 1990, 1991, 1992, 1993
@@ -26,7 +26,7 @@ All Rights Reserved.
 >>#
 >># Project: VSW5
 >>#
->># File: tset/SHAPE/tshpcrect/tshpcrect.m
+>># File: tset/SHAPE/XShapeCombineRectangles/XShapeCombineRectangles.m
 >>#
 >># Description:
 >>#     Tests for XShapeCombineRectangles()
