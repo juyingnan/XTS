01699078765acd902556b01fa2549a96eae70461
Peter Hutterer <peter.hutterer@who-t.net>
2009-09-10 13:16:14 +1000

    Renaming txtextnt16/ to XTextExtents16

--- xts5/tset/Xlib9/XTextExtents16/XTextExtents16.m
+++ xts5/tset/Xlib9/XTextExtents16/XTextExtents16.m
@@ -17,14 +17,14 @@ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
 LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
-$Header: /cvs/xtest/xtest/xts5/tset/Xlib9/txtextnt16/txtextnt16.m,v 1.2 2005-11-03 08:43:59 jmichael Exp $
+$Header: /cvs/xtest/xtest/xts5/tset/Xlib9/XTextExtents16/XTextExtents16.m,v 1.2 2005-11-03 08:43:59 jmichael Exp $
 
 Copyright (c) Applied Testing and Technology, Inc. 1995
 All Rights Reserved.
 
 >># Project: VSW5
 >># 
->># File: xts5/tset/Xlib9/txtextnt16/txtextnt16.m
+>># File: xts5/tset/Xlib9/XTextExtents16/XTextExtents16.m
 >># 
 >># Description:
 >># 	Tests for XTextExtents16()
