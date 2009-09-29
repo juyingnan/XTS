7d9596a565a7f7d30cac7ac1fec6dbcbf685ce4e
Dan Nicholson <dbn.lists@gmail.com>
2009-09-29 06:17:55 -0700

    xts5: Flatten XtE directory more

--- xts5/XtE/StringDefs.h.m
+++ xts5/XtE/StringDefs.h.m
@@ -25,7 +25,7 @@ All Rights Reserved.
 >># 
 >># Project: VSW5
 >># 
->># File: xts/XtE/StringDefs.h/StringDefs.h.m
+>># File: xts/XtE/StringDefs.h.m
 >># 
 >># Description:
 >>#	Tests for StringDefs.h contents
