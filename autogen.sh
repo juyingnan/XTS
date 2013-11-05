740179e8eb150a0c786bcc33612eeadad7d80981
Super-User <root@crazyhorse.cn.oracle.com>
2013-11-05 12:53:52 +0800

    after make

--- autogen.sh
+++ autogen.sh
@@ -9,4 +9,8 @@ cd $srcdir
 autoreconf -v --install || exit 1
 cd $ORIGDIR || exit $?
 
+sed -e 's/ac_default_prefix=\/usr\/local/ac_default_prefix=\/usr/g' ./configure > ./temp
+mv ./temp ./configure
+chmod +x ./configure
+
 $srcdir/configure --enable-maintainer-mode "$@"
